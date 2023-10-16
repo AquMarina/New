43cc4a6d2f7e30e1fd1f52d64a1a7f7467a2c09b
Aquamarine <art.kep@yandex.ru>
2023-10-16 16:16:04 +0600

    Magic

---
+++ New007_Magic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 20, ya = 1, xb = 1, yb = 15, xc = 40, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 10;
